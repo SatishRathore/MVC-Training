faf62d41fcaab74557aeccae0a6e97158a07a255
Satish Rathore <satish.rathore@morningstar.com>
2016-04-26 16:38:12 +0530

    Unit Test Added

---
+++ MVC.UnitTest.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC.UnitTest;
+using MVC.UnitTest.Controllers;
+
+namespace MVC.UnitTest.Tests.Controllers
+{
+	[TestClass]
+	public class HomeControllerTest
+	{
+
+		#region ViewResult Test
+		
+		[TestMethod]
+		public void About()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.About() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void Contact()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Contact() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		#endregion
+
+		#region ViewBag Test
+
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+		}
+
+		#endregion
+
+		#region RedirectResult Test
+
+		[TestMethod]
+		public void TestDetailsForRedirect()
+		{
+			HomeController controller = new HomeController();
+			var result = controller.Details(-1) as RedirectToRouteResult;
+			Assert.AreEqual("Index", result.RouteValues["action"]); //action method name
+			Assert.AreEqual("SomeElse", result.RouteValues["controller"]); // controller name
+		}
+
+
+		[TestMethod]
+		public void TestDetailsForViewResult()
+		{
+			HomeController controller = new HomeController();
+			ViewResult result1 = (ViewResult)controller.Details(2);
+			Assert.AreEqual("Details", result1.ViewName);
+		}
+
+		#endregion
+	}
+}
+
+
---
+++ MVC.UnitTest/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.UnitTest.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your app description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		public ActionResult GetView(int id)
+		{
+			if (id == 0)
+			{
+				return View("View2");
+			}
+			else
+			{
+				return View("View1");
+			}
+		}
+
+
+		public ActionResult Details(int Id)
+		{
+			if (Id < 0)
+			{
+				return RedirectToAction("Index", "SomeElse");
+			}
+			else
+			{
+				return View("Details");
+			}
+
+		}
+	}
+}
